a1b66f37de53ec1083b0ba821c9b0575ebb431fc
michael <mvachette@nuxeo.com>
2021-08-02 11:56:35 +0900

    Add Custom Filter

---
+++ nuxeo-resource-rest-api-core/src/main/java/org/nuxeo/ecm/restapi/server/jaxrs/filter/CustomAPIFilter.java
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2021 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Michael Vachette
+ */
+
+package org.nuxeo.ecm.restapi.server.jaxrs.filter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAPIFilter implements Filter {
+
+    public static final String CUSTOM_FILTER_RESPONSE_HEADER = "custom-filter-response-header";
+
+    private static final Log log = LogFactory.getLog(CustomAPIFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        // nothing to do
+    }
+
+    @Override
+    public void destroy() {
+        // nothing to do
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader(CUSTOM_FILTER_RESPONSE_HEADER, "abcd");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
